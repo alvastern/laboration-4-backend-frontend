d6d5bf46d032a42e5f8f3a6054084c059606c6a7
alvastern <alva.stern02@gmail.com>
2026-05-08 16:35:43 +0200

    Skapade protected.js där användaren skickas tillbaka till logga in om inte token stämmer

---
+++ src/js/protected.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const token = localStorage.getItem("token");
+
+if (!token) {
+    window.location.href = "/index.html";
+}
+
+const response = await fetch("http://localhost:3000/api/protected", {
+    method: "GET",
+    headers: {
+        "Authorization": `Bearer ${token}`
+    }
+});
+
+const data = await response.json();
+document.getElementById("message").textContent = data.message;
